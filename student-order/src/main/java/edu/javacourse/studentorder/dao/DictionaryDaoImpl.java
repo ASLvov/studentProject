e8af4dbda22cc4f8bf0c0b7a21dea8f8219d126e
Александр Львов <Lvov_AS@corp.fcorenburg.com>
2020-11-18 10:33:27 +0500

    add unit-test, auto-filling DB

--- student-order/src/main/java/edu/javacourse/studentorder/dao/DictionaryDaoImpl.java
+++ student-order/src/main/java/edu/javacourse/studentorder/dao/DictionaryDaoImpl.java
@@ -1,13 +1,15 @@
 package edu.javacourse.studentorder.dao;
 
-import edu.javacourse.studentorder.config.Config;
 import edu.javacourse.studentorder.domain.CountryArea;
 import edu.javacourse.studentorder.domain.PassportOffice;
 import edu.javacourse.studentorder.domain.RegisterOffice;
 import edu.javacourse.studentorder.domain.Street;
 import edu.javacourse.studentorder.exception.DaoException;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -21,13 +23,8 @@ public class DictionaryDaoImpl implements DictionaryDao {
     private static final String GET_AREA = "SELECT * " +
             "FROM jc_country_struct WHERE area_id like ? and area_id <> ?";
 
-    //TODO refactoring - make one method
     private Connection getConnection() throws SQLException {
-        Connection con = DriverManager.getConnection(
-                Config.getProperty(Config.DB_URL),
-                Config.getProperty(Config.DB_LOGIN),
-                Config.getProperty(Config.DB_PASSWORD));
-        return con;
+        return ConnectionBuilder.getConnection();
     }
 
     public List<Street> findStreets(String pattern) throws DaoException {
